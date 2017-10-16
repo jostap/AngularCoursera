6b2526b25931bcb13ff3a456c8aba67d9138f3ad
Jose Tapia <jose.rikleo.tapia@gmail.com>
2017-10-17 00:29:26 +0200

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,21 @@ import { Component, OnInit, Inject } from '@angular/core';
 
 import { Leader}  from './../shared/leader';
 import { LeaderService } from './../services/leader.service';
-import { LEADERS } from './../shared/leaders';
+//import { LEADERS } from './../shared/leaders';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[];
